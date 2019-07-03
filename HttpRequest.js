28b1a05b0f0ad003e0daf5aef2612489090d5c01
Tonmoy Nandy <tonmoy@matrixnmedia.com>
2019-07-03 17:31:37 +0530

    first commit

---
+++ HttpRequest.js
@@ -0,0 +1,77 @@
+/*!
+ * Bins HttpRequest
+ *
+ * Copyright BinsSoft
+ * Released under the MIT license
+ *
+ * Date: 2019-07-03
+ */
+class HttpRequest {
+
+
+    constructor(optionData) {
+        this.apiBaseUrl = '';
+        this.baseHeaders = {};
+        this.responsedataType = 'JSON';
+        this.successEvent;
+        this.beforeSendEvent;
+        this.apiBaseUrl = optionData.baseUrl;
+        if (optionData.headers) {
+            this.baseHeaders = optionData.headers;
+        }
+        if (optionData.responseDataType) {
+            this.responsedataType = optionData.responseDataType;
+        }
+        this.responsedataType = this.responsedataType.toLowerCase();
+    }
+    setConfig(configParamName, paramValue) {
+        if (configParamName === 'headers') {
+            if (typeof paramValue === 'object') {
+                this.baseHeaders = paramValue;
+            }
+        }
+    }
+    setHeader(paramName, paramValue) {
+        this.baseHeaders[paramName] = paramValue;
+    }
+    call(endPoint, method, headers = null, body = null) {
+        if (endPoint) {
+            let url = this.apiBaseUrl + endPoint;
+            let responseType = this.responsedataType
+            if (method) {
+                method = method.toUpperCase();
+                return new Promise((resolve, reject) => {
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.open(method, url, true);
+                    let localHeaders = null;
+                    if (headers) {
+                        for (let headerKey in headers) {
+                            this.baseHeaders[headerKey] = headers[headerKey];
+                        }
+                    }
+                    for (let headerKey in this.baseHeaders) {
+                        xhttp.setRequestHeader(headerKey, this.baseHeaders[headerKey]);
+                    }
+
+                    xhttp.responseType = this.responsedataType;
+                    xhttp.send(body);
+                    xhttp.onreadystatechange = function () {
+                        if (this.readyState == 4) {
+                            if (this.status == 200) {
+                                resolve(this.response);
+                            } else {
+                                resolve(this.statusText);
+                            }
+                        }
+                    }
+                });
+
+            } else {
+                console.error('method is not provided');
+            }
+
+        } else {
+            console.error('Api end point is not provided');
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # httprequest
+![Support](https://img.shields.io/badge/Support-javascript%20-blue.svg?style=flat-square) ![Support](https://img.shields.io/badge/Support-es6%20-blue.svg?style=flat-square) ![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+It is a HTTP request plugin with synchronize way.
+
+# Install
+
+    <script src="/path/to/HttpRequest.js"></script>
+
+# Implementation
+
+### Initialization
+
+    const  http = new  HttpRequest({
+      'baseUrl':  'http://localhost:8000/api/v1/',
+      'responseDataType':  'JSON'
+    });
+
+### API call
+
+#### GET
+
+    const listApiCall = http.call('get-list-data', 'GET');
+    listApiCall.then((responseData)=>{
+    
+      ... do something ...
+    
+    });
+#### SET Headers
+
+    http.setHeader('x-access-token', '<token data>');
+    http.setHeader('Content-Type', '<content type>');
+    
+    .... more on ...
+
+#### POST
+
+    http.call('authentication', 'POST', null, JSON.stringify({
+    "email":"test@test.com",
+    "password":"test@123"
+    })).then((responseData)=>{
+    
+      ... do something ...
+    
+    });
+   
+## Creator
+#### Tonmoy Nandy
+
+## License
+
+#### The MIT License (MIT)
